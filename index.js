c393e87c7440782e6ec215bb597e79747029836a
Frankie Bagnardi <f.bagnardi@gmail.com>
2018-01-28 06:28:51 -0700

    attempts to migrate to 'class' components (without class syntax)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var loaderUtils = require("loader-utils");
+var outdent = require('outdent');
 
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
@@ -6,25 +7,36 @@ module.exports.pitch = function(remainingRequest) {
 	var query = loaderUtils.parseQuery(this.query);
 	var moduleRequest = "!!" + remainingRequest;
 
-	return [
-		'var React = require("react");',
-		'var component;',
-		'var desc = {',
-		'	loadComponent: function(callback) {',
-		'		if(!component) {',
-		'			require.ensure([], function() {',
-		'				component = require(' + loaderUtils.stringifyRequest(this, moduleRequest) + ');',
-		'				if (component && component.default) component = component.default;',
-		'				if(callback) callback(component);',
-		'			}' + (query.name ? ', ' + JSON.stringify(query.name) : '') + ');',
-		'		} else if(callback) callback(component);',
-		'		return component;',
-		'	},',
-		'};',
-		'var mixinReactProxy = require(' + loaderUtils.stringifyRequest(this, require.resolve("./mixinReactProxy")) + ');',
-		'mixinReactProxy(React, desc);',
-		'module.exports = React.createClass(desc);',
-    'module.exports.requestPreload = function requestPreload() { desc.loadComponent(function() {}) };',
-		'module.exports.Mixin = desc;'
-	].join("\n");
+  return outdent`
+    var React = require('react');
+    var RComponent = React.Component;
+    var component;
+
+    function C(props) {
+      RComponent.call(this, props);
+      this.isUnmounted = false;
+      this.state = this._getInitialState();
+    }
+    Object.defineProperty(C, 'name', { value: 'ReactProxyComponent' });
+    var mixin = requre(${loaderUtils.stringifyRequest(this, require.resolve("./mixinReactProxy"))});
+
+    C.prototype = Object.create(RComponent.prototype);
+    mixin(React, C.prototype);
+
+    C.prototype.loadComponent = function(callback) {
+      if (!component) {
+        require.ensure([], function() {
+          component = require(${loaderUtils.stringifyRequest(this, moduleRequest)});
+          if (component && component.default) component = component.default;
+          if (callback) callback(component);
+        });
+      } else if (callback) {
+        callback(component);
+      }
+      return component;
+    };
+
+    C.requestPreload = function requestPreload() { C.prototype.loadComponent(function() {});
+    module.exports = C;
+  `;
 };
